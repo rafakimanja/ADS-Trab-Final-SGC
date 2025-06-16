062364de1f9813b1c0485d63cd2908522fb09d83
rafakimanja <rafaellopes.ifrs@gmail.com>
2025-06-16 20:04:38 -0300

    primeiro commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var (
+	porta = "5000"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello Word em Golang, rodando na porta: "+porta)
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+
+	fmt.Printf("Servidor ligado na porta %s", porta)
+	http.ListenAndServe(":"+porta, nil)
+}
